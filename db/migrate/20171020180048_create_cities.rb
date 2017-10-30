75606de9bf381fe9a858a92ed5a0505293692338
David Schaaf <david.p.schaaf@gmail.com>
2017-10-30 15:39:24 -0700

    Cleaning up the migration and adding content

--- db/migrate/20171020180048_create_cities.rb
+++ db/migrate/20171020180048_create_cities.rb
@@ -10,19 +10,42 @@ class CreateCities < ActiveRecord::Migration[5.1]
       t.belongs_to :state, index: true
       t.string :name
     end
-    add_foreign_key :counties, :states
 
     create_table :cities do |t|
       t.belongs_to :county, index: true
       t.belongs_to :state, index: true
       t.string :name
       t.string :zip_codes, array: true, default: []
-      t.decimal :latitude
-      t.decimal :longitude
       t.integer :population
       t.timestamps
     end
+
+    create_table :seo_landing_pages do |t|
+      t.belongs_to :state
+      t.belongs_to :county
+      t.belongs_to :city
+      t.string :path
+      t.timestamps
+    end
+
+    create_table :lat_longs do |t|
+      t.belongs_to :city
+      t.decimal :latitude
+      t.decimal :longitude
+      t.timestamps
+    end
+
+    add_foreign_key :counties, :states
+    
     add_foreign_key :cities, :counties
     add_foreign_key :cities, :states
+    
+    add_foreign_key :seo_landing_pages, :cities
+    add_foreign_key :seo_landing_pages, :counties
+    add_foreign_key :seo_landing_pages, :states
+    
+    add_foreign_key :lat_longs, :cities
+
+    add_index :seo_landing_pages, :path
   end
 end
